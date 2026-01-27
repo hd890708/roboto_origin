89a54327ea6b8fd276970966ea4f10df0808bcd4
tcmofashi <mofashiforzbx@qq.com>
2026-01-27 09:05:59 +0000

    Squashed 'modules/atom01_deploy/src/imu/' content from commit d462911
    git-subtree-dir: modules/atom01_deploy/src/imu
    git-subtree-split: d4629116be943fb3ffedb88847a94665dc08cabc

---
+++ src/drivers/hipnuc/hipnuc_imu_driver.cpp
@@ -0,0 +1,91 @@
+#include "hipnuc_imu_driver.hpp"
+
+HipnucIMUDriver::HipnucIMUDriver(uint16_t imu_id, const std::string& interface_type, const std::string& interface, const int baudrate)
+    : IMUDriver(), imu_id_(imu_id), interface_type_(interface_type), interface_(interface) {
+    memset(&raw_, 0, sizeof(raw_));
+    memset(&sensor_data_, 0, sizeof(sensor_data_));
+    if (interface_type_ == "serial") {
+        baudrate_ = baudrate;
+        serial_ = SerialPort::open(interface_, baudrate_);
+        SerialPort::SerialCbkFunc serial_callback = std::bind(&HipnucIMUDriver::serial_rx_cbk, this, std::placeholders::_1, std::placeholders::_2);
+        serial_->set_serial_callback(serial_callback);
+    } else if (interface_type_ == "can") {
+        can_ = SocketCAN::get(interface_);
+        CanCbkFunc can_callback = std::bind(&HipnucIMUDriver::can_rx_cbk, this, std::placeholders::_1);
+        can_->add_can_callback(can_callback, imu_id_);
+        can_->set_key_extractor([](const can_frame &frame) -> CanCbkId {
+            return frame.can_id & 0x7F;
+        });
+    } else {
+        throw std::runtime_error("Hipnuc driver only support CAN and SERIAL interface");
+    }
+}
+
+HipnucIMUDriver::~HipnucIMUDriver() {
+    if (interface_type_ == "serial" && serial_) {
+        serial_->close();
+    } else if (interface_type_ == "can" && can_) {
+        can_->remove_can_callback(imu_id_);
+    }
+}
+
+void HipnucIMUDriver::can_rx_cbk(const can_frame& rx_frame) {
+    hipnuc_can_frame_t frame;
+    frame.can_id = rx_frame.can_id;
+    frame.can_dlc = rx_frame.can_dlc;
+    memcpy(frame.data, rx_frame.data, 8);
+    
+    std::unique_lock<std::shared_mutex> lock(imu_mutex_);
+    
+    int ret = hipnuc_j1939_parse_frame(&frame, &sensor_data_);
+    if (ret == 0) {
+        return;
+    }
+    
+    ret = canopen_parse_frame(&frame, &sensor_data_);
+    if (ret == 0) {
+        return;
+    }
+}
+
+void HipnucIMUDriver::serial_rx_cbk(const uint8_t* data, size_t length) {
+    std::unique_lock<std::shared_mutex> lock(imu_mutex_);
+    
+    for (size_t i = 0; i < length; i++) {
+        if (hipnuc_input(&raw_, data[i])) {
+            // Update sensor data from parsed raw data
+            sensor_data_.quat_w = raw_.hi91.quat[0];
+            sensor_data_.quat_x = raw_.hi91.quat[1];
+            sensor_data_.quat_y = raw_.hi91.quat[2];
+            sensor_data_.quat_z = raw_.hi91.quat[3];
+            
+            sensor_data_.gyr_x = raw_.hi91.gyr[0] * DEG_TO_RAD;
+            sensor_data_.gyr_y = raw_.hi91.gyr[1] * DEG_TO_RAD;
+            sensor_data_.gyr_z = raw_.hi91.gyr[2] * DEG_TO_RAD;
+            
+            sensor_data_.acc_x = raw_.hi91.acc[0] * GRA_ACC;
+            sensor_data_.acc_y = raw_.hi91.acc[1] * GRA_ACC;
+            sensor_data_.acc_z = raw_.hi91.acc[2] * GRA_ACC;
+        }
+    }
+}
+
+std::vector<float> HipnucIMUDriver::get_ang_vel() {
+    std::shared_lock<std::shared_mutex> lock(imu_mutex_);
+    return {sensor_data_.gyr_x, sensor_data_.gyr_y, sensor_data_.gyr_z};
+}
+
+std::vector<float> HipnucIMUDriver::get_quat() {
+    std::shared_lock<std::shared_mutex> lock(imu_mutex_);
+    return {sensor_data_.quat_w, sensor_data_.quat_x, sensor_data_.quat_y, sensor_data_.quat_z};
+}
+
+std::vector<float> HipnucIMUDriver::get_lin_acc() {
+    std::shared_lock<std::shared_mutex> lock(imu_mutex_);
+    return {sensor_data_.acc_x, sensor_data_.acc_y, sensor_data_.acc_z};
+}
+
+float HipnucIMUDriver::get_temperature() {
+    std::shared_lock<std::shared_mutex> lock(imu_mutex_);
+    return sensor_data_.temperature;
+}
---
+++ src/protocol/serial/serial_port.cpp
@@ -0,0 +1,132 @@
+#include "serial_port.hpp"
+
+std::shared_ptr<spdlog::logger> SerialPort::logger_ = nullptr;
+
+SerialPort::SerialPort(const std::string& interface, int baudrate) 
+    : interface_(interface), baudrate_(baudrate), fd_(-1), running_(false) {
+    init();
+}
+
+void SerialPort::init() {
+    fd_ = ::open(interface_.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd_ < 0) {
+        if (logger_) logger_->error("Failed to open serial port: {}", interface_);
+        else std::cerr << "Failed to open serial port: " << interface_ << std::endl;
+        throw std::runtime_error("Failed to open serial port: " + interface_);
+    }
+
+    struct termios tty;
+    if (tcgetattr(fd_, &tty) != 0) {
+        if (logger_) logger_->error("Failed to get serial attributes: {}", interface_);
+        else std::cerr << "Failed to get serial attributes: " << interface_ << std::endl;
+        ::close(fd_);
+        throw std::runtime_error("Failed to get serial attributes: " + interface_);
+    }
+
+    bool configured = false;
+
+    if (!configured) {
+        speed_t speed;
+        switch (baudrate_) {
+            case 9600: speed = B9600; break;
+            case 19200: speed = B19200; break;
+            case 38400: speed = B38400; break;
+            case 57600: speed = B57600; break;
+            case 115200: speed = B115200; break;
+            case 230400: speed = B230400; break;
+            case 460800: speed = B460800; break;
+            case 921600: speed = B921600; break;
+            default: speed = B115200; break; 
+        }
+
+        cfsetispeed(&tty, speed);
+        cfsetospeed(&tty, speed);
+
+        tty.c_cflag &= ~CSIZE;
+        tty.c_cflag |= CS8;
+        tty.c_cflag &= ~PARENB;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag |= (CLOCAL | CREAD);
+        tty.c_cflag &= ~CRTSCTS;
+
+        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+        tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+        tty.c_oflag &= ~OPOST;
+
+        tty.c_cc[VMIN] = 0;
+        tty.c_cc[VTIME] = 1;
+
+        if (tcsetattr(fd_, TCSANOW, &tty) != 0) {
+            if (logger_) logger_->error("Failed to set serial attributes: {}", interface_);
+            else std::cerr << "Failed to set serial attributes: " << interface_ << std::endl;
+            ::close(fd_);
+            throw std::runtime_error("Failed to set serial attributes: " + interface_);
+        }
+    }
+
+    running_ = true;
+    rx_thread_ = std::thread([this]() {
+        pthread_setname_np(pthread_self(), "serial_rx");
+        struct sched_param sp{}; sp.sched_priority = 80;
+        pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp);
+            
+        uint8_t buf[BUF_SIZE] = {0};
+        
+        while (running_) {
+            fd_set readfds;
+            struct timeval tv;
+            
+            FD_ZERO(&readfds);
+            FD_SET(fd_, &readfds);
+            tv.tv_sec = 0;
+            tv.tv_usec = 1000;  // 1ms timeout
+
+            int ret = select(fd_ + 1, &readfds, NULL, NULL, &tv);
+            if (ret < 0) {
+                if (errno == EINTR) continue;
+                if (logger_) logger_->error("select error: {}", strerror(errno));
+                else std::cerr << "select error: " << strerror(errno) << std::endl;
+                break;
+            } else if (ret == 0) {
+                continue;  // timeout
+            }
+            
+            int n = read(fd_, buf, BUF_SIZE);
+            if (n < 0) {
+                if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
+                if (logger_) logger_->error("read error: {}", strerror(errno));
+                else std::cerr << "read error: " << strerror(errno) << std::endl;
+                break; 
+            } else if (n > 0) {
+                if (callback_) {
+                    callback_(buf, n);
+                }
+            }
+        }
+    });
+}
+
+SerialPort::~SerialPort() {
+    close();
+}
+
+std::shared_ptr<SerialPort> SerialPort::open(const std::string& interface, int baudrate) {
+    return std::shared_ptr<SerialPort>(new SerialPort(interface, baudrate));
+}
+
+void SerialPort::close() {
+    running_ = false;
+    if (rx_thread_.joinable()) {
+        rx_thread_.join();
+    }
+    if (fd_ >= 0) {
+        ::close(fd_);
+        fd_ = -1;
+    }
+}
+
+void SerialPort::set_serial_callback(SerialCbkFunc callback) {
+    callback_ = callback;
+}
+
